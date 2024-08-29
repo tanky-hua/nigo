5946ad361464f68a5c862453f420749724193305
huangfangxiong <huangfangxiong@sparkaigc.cn>
2024-08-29 10:10:41 +0800

    命令修改

--- cmd/root.go
+++ cmd/root.go
@@ -13,8 +13,8 @@ func init() {
 }
 
 var rootCmd = &cobra.Command{
-	Use:   "goctl",
-	Short: "goctl 是一个快速生成单体项目的工具",
+	Use:   "nigo",
+	Short: "nigo 是一个快速生成单体项目的工具",
 	Long: `简介：
  快速生成单体项目目录结构和代码
  涉及技术框架有gin、gorm、redis、viper等
@@ -22,8 +22,8 @@ var rootCmd = &cobra.Command{
  并提供简单的User模块demo
 	欢迎使用!!！`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(`欢迎使用Goctl!!!
-			请输入goctl help查看帮助`)
+		fmt.Println(`欢迎使用Nigo!!!
+			请输入nigo help查看帮助`)
 	},
 }
 
